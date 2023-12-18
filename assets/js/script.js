e9cd544d7d1017f0585b8d80d9c2416cfbacf7d2
root <root@SALIHA-BRIK.localdomain>
2023-12-18 13:56:43 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,64 +10,85 @@ async function getReciters() {
     data.reciters.forEach(reciter => 
     chooseReciter.innerHTML += `<option value="${reciter.id}">${reciter.name}</option>`);
 
-    chooseReciter.addEventListener('change', (event) => getMoshaf(event.target.value));
+    chooseReciter.addEventListener('change',
+     (event) => getMoshaf(event.target.value));
 }    
 getReciters()
 
 async function getMoshaf(reciter) {
     const chooseMoshaf = document.querySelector('#chooseMoshaf');
-    await fetch(`${apiUrl}/reciters?language=${language}&reciter=${reciter}`);
-    const data = await response.json();
-    const moshafs = data.reciters[0].moshafs;
+    const res = await fetch(`${apiUrl}/reciters?language=${language}&reciter=${reciter}`);
+    const data = await res.json();
+    const moshafs = data.reciters[0].moshaf
     chooseMoshaf.innerHTML = '<option value="">اختر المصحف</option>';
     moshafs.forEach(moshaf => {
-        chooseMoshaf.innerHTML += `<option value="${moshaf.id}"data-server="${moshaf.server}";data-suralist="${moshaf.suralist}">${moshaf.name}</option>`;
+        chooseMoshaf.innerHTML += `<option value="${moshaf.id}"data-server=${moshaf.server}" data-surahList="${moshaf.surah_List}">${moshaf.name}</option>`;
+        console.log(moshaf);
     });
 
-    chooseMoshaf.addEventListener('change', () => {
-        const selectMoshaf = chooseMoshaf.options[chooseMoshaf.selectedIndex];
-        const surahServer = selectMoshaf.dataset.server;
-        const surahList = selectMoshaf.dataset.suralist;
+    chooseMoshaf.addEventListener('change', event => {
+        const selectedMoshaf = chooseMoshaf.options[chooseMoshaf.selectedIndex];
+        const surahServer = selectedMoshaf.dataset.server;
+        const surahList = selectedMoshaf.dataset.surahlist;
         getSurah(surahServer, surahList);
+       
     });
 }
 
 async function getSurah(surahServer, surahList) {
-    const chooseSurah = document.querySelector('#chooseSurah');
+    const chooseSurah = document.querySelector('#chooseSurah')
 
-    const res = await fetch(`https://mp3quran.net/api/v3/suwar`);
+    const res = await fetch(`https://mp3quran.net/api/v3/suwar`)
     const data = await res.json();
     const suraNames = data.suwar;
 
     surahList = surahList.split(',');
     chooseSurah.innerHTML = '<option value="">اختر السورة</option>';
     surahList.forEach(surah => {
+        const padSurah = surah.padStart(3, '0');
         suraNames.forEach(surahName => {
+            console.log(surah, surahName.id);
             if (surahName.id == surah) {
-                chooseSurah.innerHTML += `<option value="${surahName.id}">${surahName.name}</option>`;
+                chooseSurah.innerHTML += `<option value="${surahServer}${padSurah}.mp3">${surahName.name}</option>`
+                
             }
+
         });
     });
 
-    chooseSurah.addEventListener('change', () => {
-        const selectSurah = chooseSurah.options[chooseSurah.selectedIndex];
-        playSurah(selectSurah.value);
+    chooseSurah.addEventListener('change', event => {
+        const selectedSurah = chooseSurah.options[chooseSurah.selectedIndex];
+        playSurah(selectedSurah.value);
+        console.log(selectedSurah.value);
     });
 }
-
-function playSurah(surah) {
+function playSurah(surahmp3) {
     const audioPlayer = document.querySelector('#audioPlayer');
-    audioPlayer.src = `https://server8.mp3quran.net/${surah}/${surah}.mp3`;
+audioPlayer.src = surahmp3;
     audioPlayer.play();
 }
-function playLive(channel){
-    if (hls.isSupported) {
-        var video = document.getElementById('LiveVideo');
-        var hls = new hls();
+
+
+function playLive(channel) {
+    if (Hls.isSupported) { // Remove parentheses here
+        var video = document.getElementById('livevideo');
+        var hls = new Hls();
         hls.loadSource(`${channel}`);
         hls.attachMedia(video);
-        hls.on(hls.Events.MANIFEST_PARSED,function() {
-          video.play();
-      });
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    } else {
+        // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+        // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
+        // This is using the built-in support of the plain video element, without using hls.js.
+        var video = document.getElementById('livevideo');
+        if (video.canPlayType('application/vnd.apple.mpegurl')) {
+            video.src = 'https://d26g5bnklkwsh4.cloudfront.net/adf856d9-a233-484b-a730-f7ac57c53aa5/master.m3u8';
+            video.addEventListener('canplay', function () {
+                video.play();
+            });
+        }
     }
 }
+
